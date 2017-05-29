bc834c0b4c0ad88bf40317f4c4f887bd5ab5f9df
tm731531 <tm731531@hotmail.com>
2017-05-29 17:03:53 +0800

    Add

---
+++ viewAge/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ViewageConfig(AppConfig):
+    name = 'viewAge'
---
+++ viewAge/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+from django.conf import settings
+import pymssql
+# Create your views here.
+#def hello_world(request):
+#    return HttpResponse("Hello World!")
+ 
+def hello_world(request):
+    conn = pymssql.connect(settings.APP_SETTING["serverconfig"], settings.APP_SETTING["userconfig"], settings.APP_SETTING["passwordconfig"], settings.APP_SETTING["dbconfig"])
+    cursor = conn.cursor()
+    cursor.execute("  select  *   FROM [Farmdata].[dbo].[patentFSecure] " )
+
+    row=cursor.fetchall()
+    data=[]
+    for x in row:
+        data.append({'title':x[1]})
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+        'rowdata': data,
+    })
+
+def viewpage(request):
+    
+    conn = pymssql.connect(settings.APP_SETTING["serverconfig"], settings.APP_SETTING["userconfig"], settings.APP_SETTING["passwordconfig"], settings.APP_SETTING["dbconfig"])
+    cursor = conn.cursor()
+    cursor.execute("  select  *   FROM [Farmdata].[dbo].[patentFSecure] " )
+    row=cursor.fetchall()
+    data=[]
+    for x in row:
+        data.append({'title':x[1],'pub_date': datetime.now(),'body':x[3]})
+    return render(request, 'index.html', {
+        'now': str(datetime.now()),
+        'posts': data,
+    })
+
+def viewpageConfig(request):
+    #server = getattr(settings, "serverconfig", None)
+    #user = getattr(settings, "userconfig", None)
+    #password = getattr(settings, "passwordconfig", None)
+    #data = getattr(settings, "dbconfig", None)
+    server= settings.APP_SETTING["serverconfig"]
+    user= settings.APP_SETTING["userconfig"]
+    password= settings.APP_SETTING["passwordconfig"]
+    data= settings.APP_SETTING["dbconfig"]
+    return render(request, 'viewpageConfig.html', {
+        'server': server,
+        'user': user,
+        'password': password,
+        'data': data,
+    })
+    
